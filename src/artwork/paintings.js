4e95aece62146b18a9944dcc2ddee122e15bcbf3
Mohammed Hasan Ahmed <captainhasan24@gmail.com>
2026-04-14 14:25:27 +0530

    initial project setup

---
+++ src/artwork/paintings.js
@@ -0,0 +1,12 @@
+const paintings = [
+    {
+        img: 'crafts/monalisa.jpg',
+        title: 'Mona Lisa',
+        description: " The Mona Lisa is a half-length portrait painting by the Italian Renaissance artist Leonardo da Vinci. It is considered one of the most famous and recognizable paintings in the world, known for its enigmatic smile and detailed background.",
+        category: "Renaissance"
+    }
+];
+
+
+
+export default paintings;
